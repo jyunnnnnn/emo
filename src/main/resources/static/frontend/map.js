85455717521573cedcffbb8623b288b8d102d9cd
AnitaX1018 <yujing168100@gmail.com>
2023-12-21 01:51:51 +0800

    feat: 碳足跡管理員api
    只修改資料庫 本地暫無

--- src/main/resources/static/frontend/map.js
+++ src/main/resources/static/frontend/map.js
@@ -990,6 +990,7 @@ function showNewRecord(sortedRecords) {
 // 打開管理員介面
 function showFPdata() {
     var thisData = FootprintData;
+    //console.log(thisData);
     var container = document.getElementById("adminData");
     container.innerHTML = ""; // 清空容器內容
     container.style.overflowY = "scroll";
@@ -1001,6 +1002,7 @@ function showFPdata() {
         var input = document.createElement('input');
         input.type = 'checkbox';
         input.className = 'custom-checkbox';
+        input.id = thisData[i].fpid;
         var span = document.createElement('span');
         span.className = 'checkmark';
         checkbox.appendChild(input);
@@ -1027,13 +1029,71 @@ function showFPdata() {
         footprintInput.value = thisData[i].coefficient;
         footprintInput.className = 'inputFP';
         footprintInput.disabled = true;
+        footprintInput.id = thisData[i].fpid;
 
         footprintDiv.appendChild(checkbox);
         footprintDiv.appendChild(typeInput);
         footprintDiv.appendChild(footprintInput);
         container.appendChild(footprintDiv);
     }
+    $('#saveFP').click(updateAllFootprint);//一次修改Footprint
+    $('#deleteFP').click(deleteSelectedFootprints);
 }
+
+//修改footprint
+function updateAllFootprint() {
+    var footprints = $('.inputFP');
+    //console.log(footprints);
+    footprints.each(function(index) {
+        var type = footprints.eq(index).val();
+        var coefficient = footprints.eq(index + 1).val();
+        var id = footprints.eq(index + 1).attr('id');
+
+        if (index % 2 === 0) {
+            // 使用 AJAX 送出 POST 請求給後端
+            $.ajax({
+                type: 'PUT',
+                url: `/api/updateFootprint?FPId=${id}&coefficient=${coefficient}`,
+                contentType: 'application/json',
+                success: function (response) {
+                    // 成功處理回傳的資料
+                    //console.log('FP更新成功:', response);
+                },
+                error: function (xhr, status, error) {
+                    // 處理錯誤
+                    //console.error('FP更新失敗:', error);
+                }
+            });
+        }
+    });
+}
+
+//刪除footprint
+function deleteSelectedFootprints() {
+    $('.custom-checkbox').each(function(index, checkbox) {
+        if (checkbox.checked) {
+            var footprintID = $(checkbox).attr('id');
+            // 將收集到的被勾選的 ID 發送到後端進行刪除操作
+            console.log(footprintID);
+            $.ajax({
+                type: 'DELETE',
+                url: `/api/deleteOneFootprint?FPId=${footprintID}`, // 替換成適當的後端路由
+                contentType: 'application/json',
+                success: function(response) {
+                    // 刪除成功，處理回應
+                    console.log('成功刪除:', response);
+                    // 重新載入或更新資料
+                    // 例如，重新呼叫 showFPdata() 或更新資料列表
+                },
+                error: function(xhr, status, error) {
+                    // 處理錯誤
+                    console.error('刪除失敗:', error);
+                }
+            });
+        }
+    });
+}
+
 
 //刪除Emo_User
 function deleteAccountToBackend(userId){
