4ae4e6905b2de9dc5b69945ea75d2550e7b70ff8
AnitaX1018 <yujing168100@gmail.com>
2024-02-01 00:35:53 +0800

    fix: 合併func

--- src/main/resources/static/frontend/routeRecord.js
+++ src/main/resources/static/frontend/routeRecord.js
@@ -4,15 +4,15 @@ let mapLines = [];//一次紀錄的路線線段
 let isRecording = false;//false=>開始  true=>結束
 
 function success(pos){
-    distanceThreshold = 0.010; // 十公尺
+    distanceThreshold = 10; // 十公尺
     //console.log(pos,currentLocation);
     const newLat = pos.coords.latitude;
     const newLng = pos.coords.longitude;
     const point1 = new google.maps.LatLng(newLat, newLng);
     const point2 = new google.maps.LatLng(currentLocation.lat, currentLocation.lng);
-    //計算新位置和當前位置的距離
+    //計算新位置和當前位置的距離 meter
     const distance = google.maps.geometry.spherical.computeDistanceBetween(point1, point2);
-    // 只有當距離超過閾值時才更新位置和圓圈 (小於五公尺不更新)
+    // 只有當距離超過閾值時才更新位置和圓圈 (小於10公尺不更新)
     if (distance > distanceThreshold) {
         currentLocation = {
             lat: newLat,
@@ -20,28 +20,17 @@ function success(pos){
         };
         updateCurrentCircle(pos);
     }
-
-    //有無改變位置
-    // if (newLat !== currentLocation.lat || newLng !== currentLocation.lng) {
-    //     currentLocation = {
-    //         lat: newLat,
-    //         lng: newLng
-    //     };
-    //     updateCurrentCircle(pos);
-    // }
-    // 重新啟動位置監測
-    //watchId = navigator.geolocation.watchPosition(success, error, options);
 }
 
 function error(err) {
     console.error(`ERROR(${err.code}): ${err.message}`);
     navigator.geolocation.clearWatch(watchId);//停止監測
-    watchId = navigator.geolocation.watchPosition(success, error, options);
+    watchId = navigator.geolocation.watchPosition(success, error, options);//重新監測
 }
 
 options = {
-    enableHighAccuracy: true,//高精準，耗能
-    timeout: 5000,
+    enableHighAccuracy: false,//低精準，較不耗能
+    timeout: 5000,//最長等待時間五秒
     maximumAge: 1000,//緩存位置1秒
 };
 // 路線紀錄(開始/停止)
@@ -75,7 +64,7 @@ function stopRecording() {
     //好像?抓mapLines就可以直接出現線條(還未確定，等資料庫可新增這筆在測試)
     //存kilometer
 
-    console.log(mapLines);
+    //console.log(mapLines);
     console.log("kilometer: "+kilometer.toFixed(3)+" KM");
     // 清除時間間隔
     clearInterval(intervalId);
@@ -131,19 +120,14 @@ $('#saveTrafficRecord').on('click', function () {
 function recordLocation() {
     // 儲存記錄的位置
     recordedPositions.push(currentLocation);
-    //console.log(currentLocation);
-    // 在記錄的位置之間繪製線條
-    drawLines();
-}
 
-function drawLines() {
+    // 在記錄的位置之間繪製線條
     if (recordedPositions.length >= 2) {
         //一段一段畫
         let lastTwoPoints = recordedPositions.slice(-2); // 取得最後兩個點
         let lineCoordinates = lastTwoPoints.map(function (position) {
             return new google.maps.LatLng(position.lat, position.lng);
         });
-
         let line = new google.maps.Polyline({
             path: lineCoordinates,
             geodesic: true,
@@ -153,12 +137,12 @@ function drawLines() {
         });
 
         line.setMap(map);
-
         mapLines.push(line);
-        //累加計算兩點之間距離
+        //累加計算兩點之間距離 KM
         kilometer += (google.maps.geometry.spherical.computeDistanceBetween(lastTwoPoints[0],lastTwoPoints[1])/1000);
     }
 }
+
 //清線
 function clearMapLines() {
     for (let i = 0; i < mapLines.length; i++) {
