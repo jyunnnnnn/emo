908e353dd3c5c3f39a098ba5a2d8b12b5e99c8ab
Mosmochi <sw710407@gmail.com>
2024-02-21 21:34:09 +0800

    feat:新增/更新設定檔中減碳項目

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,18 +2,22 @@ package com.example.demo;
 
 
 import com.example.demo.service.AESEncryption;
+import com.example.demo.service.ConfigService;
+import com.example.demo.service.RecordItem;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.SpringBootVersion;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 
-
 @SpringBootApplication
 //@ComponentScan(basePackages = "com.example.demo")
 public class DemoApplication {
-	public static void main(String[] args) throws Exception {
-		SpringApplication.run(DemoApplication.class, args);
-	}
+    public static void main(String[] args) throws Exception {
+//		SpringApplication.run(DemoApplication.class, args);
+        ConfigService configService = new ConfigService();
+        RecordItem newItem = new RecordItem("car", "DiaryProduct", 1.02, "KgCo2", "plastic", null);
+        configService.updateNewContent("daily", newItem);
+    }
 
 }
--- src/main/java/com/example/demo/service/ConfigService.java
+++ src/main/java/com/example/demo/service/ConfigService.java
@@ -4,14 +4,16 @@ import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import org.springframework.stereotype.Service;
 
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.PrintStream;
+import java.io.*;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 //設定檔Service物件
 @Service
 public class ConfigService {
-
+    public static int CATEGORY_NOT_EXIST = 1;
+    public static int OK = 0;
     //Gson檢析物件創建
     private GsonBuilder builder = new GsonBuilder();
     private Gson gson = builder.setPrettyPrinting().create();
@@ -30,14 +32,58 @@ public class ConfigService {
     }
 
 
-
     //獲取所有減碳紀錄事項Json字串
     public String getAllRecordJson() {
         return gson.toJson(record.getRecordCategory());
     }
 
+    //新增或更新一個新(舊)紀錄項目到特定類別
+    public int updateNewContent(String category, RecordItem newItem) throws IOException {
+        //更新設定檔內容物件
+        Map<String, RecordWrapper> t1 = record.getRecordCategory();//獲取原減碳紀錄內容
+        //不存在此類別
+        if (!t1.containsKey(category)) {
+            return CATEGORY_NOT_EXIST;
+        }
+
+        RecordWrapper t2 = t1.get(category);//獲取目標類別
+
+        List<RecordItem> content = t2.getContent();
+
+        //遍歷目標項目是否存在
+        for (RecordItem element : content) {
+            if (element.getIndex() == newItem.getIndex()) {//將目標項目從List中移除
+                content.remove(element);
+                break;
+            }
+        }
+        content.add(newItem);
+        t2.setContent(content);
+        t1.replace(category, t2);
+        record.setRecordCategory(t1);
+        //修改設定檔文件
+        String newJsonStr = gson.toJson(record);
+        updateConfiguration(newJsonStr);
+
+        return OK;
+    }
+
     public String test() {
         return gson.toJson(record.getSetting());
+    }
+
+
+    //寫入設定檔
+    private void updateConfiguration(String newJsonString) {
+        File file = new File(jsonPath);
+        BufferedWriter writer = null;
+        try {
+            writer = new BufferedWriter(new FileWriter(file));
+            writer.write(newJsonString);
+            writer.close();
+        } catch (IOException err) {
+            throw new RuntimeException(err);
+        }
     }
 
 
