c04ca0fb53d7e94b951f8c6cf049d515317aa34f
jyun <andrew98311@gmail.com>
2024-04-28 17:02:13 +0800

    fix: 手勢設定

--- src/main/resources/static/frontend/mapUI.js
+++ src/main/resources/static/frontend/mapUI.js
@@ -796,3 +796,25 @@ function convertToUnit(value) {
         return "<strong>"+value.toFixed(0).replace(/\B(?=(\d{3})+(?!\d))/g, ',')+"</strong>";
     }
 }
+
+
+
+//頁面手勢設定
+document.addEventListener('gesturestart', function(event) {
+  // 阻止兩指縮放畫面
+  event.preventDefault();
+});
+let lastTouchEndTime = 0;
+document.addEventListener('touchend', (event) => {
+  const now = new Date().getTime();
+  if((now - lastTouchEndTime) <= 300) {      // 偵測時間差是否小於 300ms
+    event.preventDefault();
+  }
+  lastTouchEndTime = now;
+}, false);
+document.addEventListener('touchstart', (event) => {
+  if (event.touches && event.touches.length > 1) {  // 禁止多指觸控
+     event.preventDefault();
+  }
+}, { passive: false });
+
